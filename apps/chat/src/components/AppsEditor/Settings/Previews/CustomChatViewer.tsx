53b3bba9397a9f77d20aa0a31a6cbd9674f474c0
Valery <valery_dluski@epam.com>
2025-03-05 12:45:29 +0100

    fix(chat): update conversation state after successful dispatch

--- apps/chat/src/components/AppsEditor/Settings/Previews/CustomChatViewer.tsx
+++ apps/chat/src/components/AppsEditor/Settings/Previews/CustomChatViewer.tsx
@@ -89,6 +89,12 @@ export const CustomChatViewer: React.FC<Props> = ({
               modelId: conversation.model.id,
             }),
           );
+          dispatch(
+            ConversationsActions.updateConversationSuccess({
+              id: conversation.id,
+              conversation,
+            }),
+          );
         }
       }
     },
